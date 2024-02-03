2c6072da1c3200d1aca226e38ba3fbfa99f49d3f
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-03 23:58:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
+	let first = document.getElementsByName("fname").value;
+	let last = document.getElementsByName("lname").value;
+	alert(`${first} ${last}`)
     //Write your code here
 
 }
